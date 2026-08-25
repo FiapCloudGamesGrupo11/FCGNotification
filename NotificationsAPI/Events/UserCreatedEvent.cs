b7c0ff31ef3f06aafe7b41047d90edd530f1fcfa
André Felipe Costa <andre.costa@pro4tech.com.br>
2026-08-25 17:34:42 -0300

    Reapply "feat: padroniza contrato de usuário e corrige contexto Docker"
    This reverts commit d58020254c2b27a939617b35bc98a95e8cbdf9ea.

--- NotificationsAPI/Events/UserCreatedEvent.cs
+++ NotificationsAPI/Events/UserCreatedEvent.cs
@@ -1,13 +1,10 @@
-using System.Text.Json.Serialization;
-
 namespace NotificationsAPI.Events;
 
 public class UserCreatedEvent
 {
-	[JsonPropertyName("Id")]
-	public Guid UserId { get; set; }
+    public Guid UserId { get; set; }
 
-	public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
-	public string Email { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
